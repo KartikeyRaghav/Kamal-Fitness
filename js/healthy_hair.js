aac5086776802b506e83b9f87e1cc34b699332f4
Kartikey Raghav <abhayjeetraghav@gmail.com>
2023-01-08 11:20:39 +0530

    Add files via upload

---
+++ js/healthy_hair.js
@@ -0,0 +1,86 @@
+export const healthy_hair = [
+    [
+        ' Healthy Hair Diet Plan Chart – Day 1',
+        '03',
+        [
+            ['Day 1', 'Diet Plan'],
+            ['Breakfast (8:00-8:30AM)', '2 capsicum omelete + 1 multigrain bread toasted + 1 cup milk'],
+            ['Mid-Meal (11:00-11:30AM)', '1 cup coconut water'],
+            ['Lunch (2:00-2:30PM)', '50gms salmon fish curry + 1 cup beans vegetable + 2 multigran chapatti + beetroot salad'],
+            ['Evening (4:00-4:30PM)', '1 orange'],
+            ['Dinner (8:00-8:30PM)', '1 cup potato and drumstick curry + 2 chapatti + salad'],
+        ]
+    ],
+    [
+        ' Healthy Hair Diet Plan Chart – Day 2',
+        '04',
+        [
+            ['Day 2', 'Diet Plan'],
+            ['Breakfast (8:00-8:30AM)', '2 beetroot parantha + 1/2 cup curd'],
+            ['Mid-Meal (11:00-11:30AM)', '1 cup pomegranate'],
+            ['Lunch (2:00-2:30PM)', '1 cup potato carrot vegetable + 2 chapatti + 1 cup curd + salad'],
+            ['Evening (4:00-4:30PM)', '1 avocado'],
+            ['Dinner (8:00-8:30PM)', '1 cup palak paneer + 2 chapatti + salad'],
+        ]
+    ],
+    [
+        ' Healthy Hair Diet Plan Chart – Day 3',
+        '05',
+        [
+            ['Day 3', 'Diet Plan'],
+            ['Breakfast (8:00-8:30AM)', '1 cup vegetable rice flakes + 1 orange'],
+            ['Mid-Meal (11:00-11:30AM)', '1 cup guava'],
+            ['Lunch (2:00-2:30PM)', '1 cup lotus stem + 2 chapatti + 1/2 cup curd + salad'],
+            ['Evening (4:00-4:30PM)', '1 cup fish pakora + green chutney'],
+            ['Dinner (8:00-8:30PM)', '1 cup arhar dal + carrot salad + 2 chapatti'],
+        ]
+    ],
+    [
+        ' Healthy Hair Diet Plan Chart – Day 4',
+        '06',
+        [
+            ['Day 4', 'Diet Plan'],
+            ['Breakfast (8:00-8:30AM)', '2 onion capsicum omelette + 1 cup low fat milk'],
+            ['Mid-Meal (11:00-11:30AM)', '1 cup watermelon'],
+            ['Lunch (2:00-2:30PM)', '1 cup pumpkin vegetable + 2 chapatti + 1 cup collard beans salad'],
+            ['Evening (4:00-4:30PM)', '1 cup sprouts salad'],
+            ['Dinner (8:00-8:30PM)', '1 cup palak paneer + 2 chapatti + tomato salad'],
+        ]
+    ],
+    [
+        ' Healthy Hair Diet Plan Chart – Day 5',
+        '07',
+        [
+            ['Day 5', 'Diet Plan'],
+            ['Breakfast (8:00-8:30AM)', '2 multigrain toasted bread + 1 spinach omelete + 1 gooseberry'],
+            ['Mid-Meal (11:00-11:30AM)', '1 cup aloe juice'],
+            ['Lunch (2:00-2:30PM)', '1 cup fish curry + 1 cup brown rice + beetroot salad'],
+            ['Evening (4:00-4:30PM)', '1 cup lemon water'],
+            ['Dinner (8:00-8:30PM)', '1 cup potato and capsicum + 2 chapatti + cucumber salad'],
+        ]
+    ],
+    [
+        ' Healthy Hair Diet Plan Chart – Day 6',
+        '08',
+        [
+            ['Day 6', 'Diet Plan'],
+            ['Breakfast (8:00-8:30AM)', '2 broccoli and egg omelette + 1 toasted wheat bread ith peanut butter'],
+            ['Mid-Meal (11:00-11:30AM)', '1 cup green tea + 1 tsp honey + 5-6 almonds'],
+            ['Lunch (2:00-2:30PM)', '1 cup baked kale with mushroom and tomato + 1 cup brown rice'],
+            ['Evening (4:00-4:30PM)', '1 cup brussel sprout salad'],
+            ['Dinner (8:00-8:30PM)', '1 cup cabbage peas + 2 chapatti + cucumber salad'],
+        ]
+    ],
+    [
+        ' Healthy Hair Diet Plan Chart – Day 7',
+        '09',
+        [
+            ['Day 7', 'Diet Plan'],
+            ['Breakfast (8:00-8:30AM)', '1 cup quinoa salad + 1 cup orange juice'],
+            ['Mid-Meal (11:00-11:30AM)', '2 apricots'],
+            ['Lunch (2:00-2:30PM)', '1 cup sweet potato curry + 1 cup brown rice + carrot salad'],
+            ['Evening (4:00-4:30PM)', '1 cup musk melon'],
+            ['Dinner (8:00-8:30PM)', '1 cup fenugreek with pea vegetable + 2 chapatti + onion salad'],
+        ]
+    ],
+];
---
+++ js/muscle_gain.js
@@ -0,0 +1,86 @@
+export const muscle_gain = [
+    [
+        'Muscle Gain Diet Plan Chart – Day 1',
+        '04',
+        [
+            ['Day 1', 'Diet Plan'],
+            ['Breakfast (8:00-8:30AM)', 'chapati-4 + Egg roast 1/2 cup 2 egg'],
+            ['Mid-Meal (11:00-11:30AM)', 'green gram sprouts 1 cup'],
+            ['Lunch (2:00-2:30PM)', '4 Roti +1/2 cup salad + Fish curry (180 gm fish) + 1/2 cup cabbage subji.'],
+            ['Evening (4:00-4:30PM)', '1 Portion fruit+ cottage cheese'],
+            ['Dinner (8:00-8:30PM)', '3 Roti/Chapati + Tomato subji 1/2 cup.'],
+        ]
+    ],
+    [
+        'Muscle Gain Diet Plan Chart – Day 2',
+        '05',
+        [
+            ['Day 2', 'Diet Plan'],
+            ['Breakfast (8:00-8:30AM)', 'Chicken sandwich (4 slice bread) + 1 cup skimmed milk.'],
+            ['Mid-Meal (11:00-11:30AM)', '1 Portion fruit salad + Cottage cheese.'],
+            ['Lunch (2:00-2:30PM)', 'Veg pulav rice 1.5 cup + 1 cup Soya Chunk curry + 1/2 cup Low fat curd.'],
+            ['Evening (4:00-4:30PM)', '1 cup light tea + Chicken salad 1 cup.'],
+            ['Dinner (8:00-8:30PM)', '3 roti/ Chapathi + Lady finger subji 1/2 cup.'],
+        ]
+    ],
+    [
+        'Muscle Gain Diet Plan Chart – Day 3',
+        '06',
+        [
+            ['Day 3', 'Diet Plan'],
+            ['Breakfast (8:00-8:30AM)', 'Sprouts & Paneer Paratha 3 + Green chutney.'],
+            ['Mid-Meal (11:00-11:30AM)', '1 cup boilled black channa.'],
+            ['Lunch (2:00-2:30PM)', '1.5 cup rice + chicken curry (150 gm chicken) + Palak subji 1/2 cup + 1/2 cup low fat curd.'],
+            ['Evening (4:00-4:30PM)', '1 Portion fruit salad + Yoghurt'],
+            ['Dinner (8:00-8:30PM)', 'Brocken wheat upma 1 cup + 1/2 cup green beans subji'],
+        ]
+    ],
+    [
+        'Muscle Gain Diet Plan Chart – Day 4',
+        '07',
+        [
+            ['Day 4', 'Diet Plan'],
+            ['Breakfast (8:00-8:30AM)', 'Soya flour Uttapam 2 +Tomato /green chutney + 1 glass skimmed milk.'],
+            ['Mid-Meal (11:00-11:30AM)', '1 Portion fruit salad + Cottage cheese.'],
+            ['Lunch (2:00-2:30PM)', '1.5 cup rice+Kidney beans curry 1 cup + 1/2 cup cucumber salad + Lady finger subji 1/2 cup.'],
+            ['Evening (4:00-4:30PM)', '1 Cup light tea + Brown rice flakes poha with nuts 1 cup.'],
+            ['Dinner (8:00-8:30PM)', 'Wheat dosa 3 + 1 cup Bitter guard subji.'],
+        ]
+    ],
+    [
+        'Muscle Gain Diet Plan Chart – Day 5',
+        '08',
+        [
+            ['Day 5', 'Diet Plan'],
+            ['Breakfast (8:00-8:30AM)', 'Mushroom Paratha 2 + Tomato chutney + Scrambled egg (2 eggs)'],
+            ['Mid-Meal (11:00-11:30AM)', 'plane Yoghurt with raw vegetables / grilled vegetables - 1 cup'],
+            ['Lunch (2:00-2:30PM)', '1/2 cup rice + 3 medium chapati + Fish masala 1 cup (fish 180 g) + Snake guard subji 1/2 cup.'],
+            ['Evening (4:00-4:30PM)', '1 cup boilled channa+ light tea 1 cup.'],
+            ['Dinner (8:00-8:30PM)', '3 Roti/ chapati + 1/2 cup mix veg curry'],
+        ]
+    ],
+    [
+        'Muscle Gain Diet Plan Chart – Day 6',
+        '09',
+        [
+            ['Day 6', 'Diet Plan'],
+            ['Breakfast (8:00-8:30AM)', '4 Idli + Sambar 1/2 cup + 1 table spoon Green chutney/ Tomato Chutney + 2 egg white'],
+            ['Mid-Meal (11:00-11:30AM)', '1 cup banana + almond milk shake.'],
+            ['Lunch (2:00-2:30PM)', '1 cup rice + Soya chunk curry 1/2 cup + Lady finger subji 1/2 cup + small cup low fat curd.'],
+            ['Evening (4:00-4:30PM)', '1 cup tea + Home made protein bar.'],
+            ['Dinner (8:00-8:30PM)', '3 Roti / chapati + Ridge guard subji 1/2 cup.'],
+        ]
+    ],
+    [
+        'Muscle Gain Diet Plan Chart – Day 7',
+        '10',
+        [
+            ['Day 7', 'Diet Plan'],
+            ['Breakfast (8:00-8:30AM)', 'Chicken Keema Paratha 2 + 1 tbs green chutney + 1 glass skim milk.'],
+            ['Mid-Meal (11:00-11:30AM)', '1 cup boilled channa'],
+            ['Lunch (2:00-2:30PM)', '4 chapati + Grilled chicken 150 gm + Dhal 1/2 cup + cucumber salad 1/2 cup.'],
+            ['Evening (4:00-4:30PM)', '1 cup blue berry milk shake'],
+            ['Dinner (8:00-8:30PM)', 'Brocken wheat upma 1 cup + 1/2 cup green beans subji'],
+        ]
+    ],
+];
